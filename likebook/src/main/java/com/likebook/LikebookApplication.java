fd25bc5c10e1a3cdfcb34c6fdd47fe83238e2292
AzureAD\정윤종 <yjjung@jellypage.com>
2023-03-06 14:25:38 +0900

    프로젝트 최초 생성 및 기본 세팅

---
+++ likebook/src/main/java/com/likebook/LikebookApplication.java
@@ -0,0 +1,12 @@
+package com.likebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikebookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LikebookApplication.class, args);
+    }
+
+}
